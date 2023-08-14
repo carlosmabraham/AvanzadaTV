44373b9b506704222bc085c00d788ddcc8e5eb70
Carlos <carlosmabraham@gmail.com>
2023-08-14 09:01:35 -0700

    Dibujo de un triangulo y una linea

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 
+/*
 ctx.fillStyle = "rgb(53, 129, 220)";
 ctx.fillRect(10, 10, 55, 55);
 
@@ -9,3 +10,17 @@ ctx.fillRect(57, 55, 55, 55);
 
 ctx.fillStyle = "rgba(245, 100, 162, 0.5)";
 ctx.fillRect(102, 100, 55, 55);
+*/
+
+ctx.fillStyle = "rgb(236, 34, 120)";
+
+ctx.beginPath();
+ctx.moveTo(185, 10);
+ctx.lineTo(185, 65);
+ctx.lineTo(125, 65);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(200, 45);
+ctx.lineTo(185, 125);
+ctx.stroke();
