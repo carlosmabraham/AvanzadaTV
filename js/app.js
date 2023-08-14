6757a524334a86d26caaf0c80a2de3b02fef46f6
Carlos <carlosmabraham@gmail.com>
2023-08-14 09:25:15 -0700

    Dibujo de dos circulos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-let canvas = document.getElementById('myCanvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-/*
+
 ctx.fillStyle = "rgb(53, 129, 220)";
 ctx.fillRect(10, 10, 55, 55);
 
@@ -10,7 +10,7 @@ ctx.fillRect(57, 55, 55, 55);
 
 ctx.fillStyle = "rgba(245, 100, 162, 0.5)";
 ctx.fillRect(102, 100, 55, 55);
-*/
+
 
 ctx.fillStyle = "rgb(236, 34, 120)";
 
@@ -23,4 +23,15 @@ ctx.fill();
 ctx.beginPath();
 ctx.moveTo(200, 45);
 ctx.lineTo(185, 125);
+ctx.stroke();
+
+
+ctx.fillStyle = "blue";
+ctx.beginPath();
+ctx.arc(135, 200, 40, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.fillStyle = "rgba(245, 100, 162, 0.5)";
+ctx.beginPath();
+ctx.arc(230, 200, 40, 0, 2 * Math.PI);
 ctx.stroke();
