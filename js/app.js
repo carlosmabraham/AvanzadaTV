0894c7311efec874991c2b0628e559b663631030
Carlos <carlosmabraham@gmail.com>
2023-08-13 23:40:57 -0700

    ejercicio dibujar 3 cuadrados

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "rgb(200, 0, 0)";
-ctx.fillRect(10, 10, 55, 50);
+ctx.fillStyle = "rgb(53, 129, 220)";
+ctx.fillRect(10, 10, 55, 55);
 
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect(30, 30, 55, 50);
+ctx.fillStyle = "rgb(236, 34, 120)";
+ctx.fillRect(57, 55, 55, 55);
+
+ctx.fillStyle = "rgba(245, 100, 162, 0.5)";
+ctx.fillRect(102, 100, 55, 55);
