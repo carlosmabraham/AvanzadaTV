34ab3aded161bebb89932e5fb151c64c928915ad
Carlos Abraham Meza Cota <carlosmabraham@gmail.com>
2023-08-26 19:10:53 -0600

    pantalones

--- js/dibujo.js
+++ js/dibujo.js
@@ -7,7 +7,5 @@ ctx.fillStyle = "#1130B8"
 ctx.fillRect(200, 300, 100, 170);
 
 
-ctx.beginPath();
-ctx.moveTo(200, 400);
-ctx.lineTo(200, 500);
-ctx.stroke();
+ctx.fillStyle = "gray";
+ctx.fillRect(240, 380, 20, 90);
