5b893488ed6ff4b9f6a5be901b7defd19dd6ca42
Carlos Abraham Meza Cota <carlosmabraham@gmail.com>
2023-08-26 22:52:06 -0600

    pies cuerpo

--- js/dibujo.js
+++ js/dibujo.js
@@ -1,11 +1,28 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-ctx.strokeRect(50, 40, 40, 40);
 
+//pantalones
 ctx.fillStyle = "#1130B8"
 ctx.fillRect(200, 300, 100, 170);
 
-
 ctx.fillStyle = "gray";
 ctx.fillRect(240, 380, 20, 90);
+
+
+
+ctx.fillStyle = "blue";
+ctx.beginPath();
+ctx.arc(135, 200, 40, 0, 2 * Math.PI);
+ctx.fill();
+
+
+//Zapatos
+ctx.fillStyle = "#BA056C"
+ctx.fillRect(200, 470, 50, 30);
+ctx.fillRect(260, 470, 50, 30);
+
+
+//Cuepo
+ctx.fillStyle = "#29BA05";
+ctx.fillRect(200, 150, 100, 150);
