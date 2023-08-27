b214be3776a0fb4ccbf95f114715c322fd932b80
Carlos <carlosmabraham@gmail.com>
2023-08-27 12:17:40 -0700

    mangas brazos

--- js/dibujo.js
+++ js/dibujo.js
@@ -10,19 +10,27 @@ ctx.fillStyle = "gray";
 ctx.fillRect(240, 380, 20, 90);
 
 
-
-ctx.fillStyle = "blue";
-ctx.beginPath();
-ctx.arc(135, 200, 40, 0, 2 * Math.PI);
-ctx.fill();
-
-
 //Zapatos
 ctx.fillStyle = "#BA056C"
 ctx.fillRect(200, 470, 50, 30);
 ctx.fillRect(260, 470, 50, 30);
 
 
-//Cuepo
+//Cuerpo
 ctx.fillStyle = "#29BA05";
 ctx.fillRect(200, 150, 100, 150);
+ctx.beginPath();
+ctx.arc(200,190,40,0,Math.PI,true);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(295,190,40,0,Math.PI,true);
+ctx.fill();
+ctx.fillStyle = "#F9D69D";
+ctx.fillRect(170, 190, 25, 90);
+ctx.fillRect(300, 190, 25, 90);
+ctx.beginPath();
+ctx.arc(183, 275, 15, 0, 2 * Math.PI);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(313, 275, 15, 0, 2 * Math.PI);
+ctx.fill();
