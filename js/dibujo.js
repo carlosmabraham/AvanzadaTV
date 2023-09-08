13634b0378237737a51d84106961f04282beab35
Carlos <carlosmabraham@gmail.com>
2023-09-08 02:06:44 -0700

    dibujo sonic

--- js/dibujo.js
+++ js/dibujo.js
@@ -1,209 +1,120 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-// Sky
-ctx.fillStyle = "rgb(154, 249, 246)";
-ctx.fillRect(0, 0, 500,500);
+// Funcion para dibujar los cuadrados
+const pintarCuadrados = () => {
+  let colorCuadrado = "rgb(211, 84, 0)";
+  let canvasWidth = canvas.width;
+  let canvasHeight = canvas.height;
 
-// Platform1
-ctx.fillStyle = "rgb(247, 169, 69)";
-ctx.fillRect(0, 451, 500, 49);
+  let anchoCuadro = canvasWidth / 20;
+  let altoCuadro = canvasHeight / 20;
 
-// Platform2
-ctx.fillStyle = "rgb(252, 196, 190)";
-ctx.fillRect(0, 430, 500, 20);
-ctx.lineWidth = 3;
-ctx.strokeStyle = "black";
-ctx.strokeRect(0, 430, 500, 20);
+  for(let j = 16; j < 20; j++) {
 
-//Block1
-ctx.lineWidth = 2;
-ctx.strokeRect(50, 200, 35, 35);
-ctx.fillStyle = "rgb(255, 141, 20)";
-ctx.fillRect(50, 200, 35, 35);
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(57,205, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(77,205, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(57,230, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(77,230, 1, 10, Math.PI);
-ctx.fill();
-//Block2
-ctx.lineWidth = 2;
-ctx.strokeRect(130, 50, 35, 35);
-ctx.fillStyle = "rgb(255, 141, 20)";
-ctx.fillRect(130, 50, 35, 35);
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(137,55, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(157,55, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(137,80, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(157,80, 1, 10, Math.PI);
-ctx.fill();
-ctx.fillStyle = "rgb(255, 221, 185)";
-ctx.fillRect(130, 60, 9, 9);
-ctx.fillRect(157, 57, 6, 6);
-//Block3
-ctx.lineWidth = 2;
-ctx.strokeRect(165, 50, 35, 35);
-ctx.fillStyle = "rgb(255, 141, 20)";
-ctx.fillRect(165, 50, 35, 35);
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(172,55, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(193,55, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(172,80, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(193,80, 1, 10, Math.PI);
-ctx.fill();
-//Block4
-ctx.lineWidth = 2;
-ctx.strokeRect(425, 110, 35, 35);
-ctx.fillStyle = "rgb(255, 141, 20)";
-ctx.fillRect(425, 110, 35, 35);
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(432,115, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(453,115, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(432,140, 1, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(453,140, 1, 10, Math.PI);
-ctx.fill();
-ctx.fillStyle = "rgb(255, 221, 185)";
-ctx.fillRect(425, 120, 9, 9);
-ctx.fillRect(453, 117, 6, 6);
+    let y = j * altoCuadro;
 
-//Platform-block1
-ctx.fillStyle = "rgb(70, 174, 213)";
-ctx.fillRect(200, 278, 80, 150);
-ctx.strokeRect(200, 278, 80, 150);
-ctx.fillStyle = "rgb(120, 187, 212)";
-ctx.fillRect(202, 280, 71, 141);
-ctx.fillStyle = "black";
-ctx.fillRect(280, 300, 8, 130);
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(210,288, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(205, 285);
-ctx.lineTo(215, 292);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(270,288, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(274, 285);
-ctx.lineTo(265, 292);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(270,417, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(274, 414);
-ctx.lineTo(265, 421);
-ctx.stroke();
+    for (let i = 0; i < 20; i++) {
+      let x = i * anchoCuadro;
+      
+      if( j % 2 != 0 ) {
+        if(i % 2 != 0) {
+          colorCuadrado = "rgb(211, 84, 0)";
+        } else {
+          colorCuadrado = "rgb(71, 30, 3)";
+        }
+      } else {
+        if(i % 2 != 0) {
+          colorCuadrado = "rgb(71, 30, 3)";
+        } else {
+          colorCuadrado = "rgb(211, 84, 0)";
+        }
+      }
+      
+      ctx.fillStyle = colorCuadrado;
+      ctx.fillRect(x, y, anchoCuadro, altoCuadro);
+    }
+  };
+};
 
-//Platform-block2
-ctx.fillStyle = "rgb(250, 197, 149)";
-ctx.fillRect(150, 338, 90, 90);
-ctx.strokeRect(150, 338, 90, 90);
-ctx.fillStyle = "rgb(247, 217, 190)";
-ctx.fillRect(152, 340, 81, 81);
-ctx.fillStyle = "black";
-ctx.fillRect(240, 360, 8, 70);
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(160,350, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(155, 347);
-ctx.lineTo(165, 354);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(230,350, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(234, 347);
-ctx.lineTo(225, 354);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(160,419, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(155, 416);
-ctx.lineTo(165, 423);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(230,419, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(234, 416);
-ctx.lineTo(225, 423);
-ctx.stroke();
+// Funcion para dibujar el pasto
+const pintarPasto = () => {
+  ctx.fillStyle = "rgb(102, 211, 0)";
+  ctx.fillRect(0, 371, 500, 15);
+  ctx.fillStyle = "rgb(77, 153, 6)";
+  ctx.fillRect(0, 386, 500, 15);
+  ctx.fillStyle = "rgb(77, 153, 6)";
+  ctx.fillRect(0, 401, 500, 15);
+  ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+  ctx.fillRect(0, 401, 500, 35);
+};
 
-//Platform-block3
-ctx.fillStyle = "rgb(15, 222, 27)";
-ctx.fillRect(410, 338, 100, 90);
-ctx.strokeRect(410, 338,100, 90);
-ctx.fillStyle = "rgb(129, 222, 135)";
-ctx.fillRect(412, 340, 91, 81);
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(422,419, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(426, 416);
-ctx.lineTo(417, 423);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(422,350, 6, 10, Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.moveTo(426, 347);
-ctx.lineTo(417, 354);
-ctx.stroke();
+// Funcion para dibujar los aros
+const pintarAro = (x, y) => {
+  ctx.strokeStyle = "rgb(238, 245, 15)";
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.arc(x, y, 20, 0, Math.PI*2);
+  ctx.stroke();
+};
 
-//Tube
-ctx.fillStyle = "rgb(12, 128, 5)";
-ctx.fillRect(328, 378, 50, 50);
-ctx.strokeRect(328, 378, 50, 50);
-ctx.fillRect(320, 347, 65, 30);
-ctx.strokeRect(320, 347, 65, 30);
+// Funcion para pintar girasol
+const pintarGirasol = () => {
+  ctx.fillStyle = "rgb(77, 153, 6)";
+  ctx.fillRect(423, 306, 7, 65);
+  ctx.fillStyle = "rgb(238, 245, 15)";
+  ctx.beginPath();
+  ctx.arc(426, 265, 40, 0, Math.PI*2);
+  ctx.fill();
+  ctx.fillStyle = "rgb(14, 106, 1)";
+  ctx.beginPath();
+  ctx.arc(426, 265, 20, 0, Math.PI*2);
+  ctx.fill();
+};
 
-//Three
-ctx.strokeStyle = "rgb(9, 101, 22)";
-ctx.fillStyle = "rgb(77, 211, 96)";
-ctx.beginPath();
-ctx.moveTo(20, 428);
-ctx.lineTo(100, 428);
-ctx.arc(90,412,20,1,1.5*Math.PI, true);
-ctx.arc(65,395,30,0,Math.PI, true);
-ctx.arc(35,413,20,15,1.5*Math.PI);
-ctx.stroke();
-ctx.fill();
+// Funcion para pintar palmera
+const pintarPalmeraCoordenadas = (x, y) => {
+  ctx.fillStyle = "rgb(110, 44, 0)";
+  ctx.fillRect(x, y, 12, 250);
+  ctx.fillStyle = "rgb(60, 95, 14)";
+  ctx.fillRect(x -100, y - 31, 100, 30);
+  ctx.beginPath();
+  ctx.moveTo(x -150, y - 31);
+  ctx.lineTo(x - 100, y + 9);
+  ctx.lineTo(x - 100, y - 31);
+  ctx.lineTo(x - 150, y - 31);
+  ctx.fill();
+  ctx.fillRect(x + 10, y - 31, 100, 30);
+  ctx.beginPath();
+  ctx.moveTo(x + 95, y - 21);
+  ctx.lineTo(x +135, y + 19);
+  ctx.lineTo(x + 145, y - 31);
+  ctx.lineTo(x + 95, y - 31);
+  ctx.fill();
+  ctx.fillRect(x - 10, y - 1, 30, 90);
+  ctx.beginPath();
+  ctx.moveTo(x - 20, y + 94);
+  ctx.lineTo(x + 30, y + 114);
+  ctx.lineTo(x + 20, y + 74);
+  ctx.lineTo(x - 20, y + 94);
+  ctx.fill();
+  ctx.fillRect(x -10, y - 81, 30, 80);
+  ctx.beginPath();
+  ctx.moveTo(x -20, y - 76);
+  ctx.lineTo(x + 30, y - 56);
+  ctx.lineTo(x + 20, y - 96);
+  ctx.lineTo(x - 20, y - 76);
+  ctx.fill();
+};
 
 
 
+
+pintarCuadrados();
+pintarPasto();
+pintarAro( 70, 335 );
+pintarAro( 160, 335 );
+pintarAro( 250, 335 );
+pintarGirasol();
+pintarPalmeraCoordenadas(320, 121);
+pintarPalmeraCoordenadas(-15, 121);
